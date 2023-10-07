98bfb336a6c5a2a6372c035a55f3c85b93d2dd52
Thomas.B <t.bagnolati@gmail.com>
2023-10-07 18:13:27 +0200

    message notify in progress

--- feature/items/src/main/kotlin/com/bagnolati/learnoflegends/feature/items/ItemsScreen.kt
+++ feature/items/src/main/kotlin/com/bagnolati/learnoflegends/feature/items/ItemsScreen.kt
@@ -1,6 +1,15 @@
 package com.bagnolati.learnoflegends.feature.items
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.bagnolati.learnoflegends.core.ui.preview.DevicesPreviews
+import com.bagnolati.learnoflegends.core.ui.preview.ThemePreviews
+import com.bagnolati.learnoflegends.core.ui.theme.LolTheme
 
 @Composable
 internal fun ItemsRoute() {
@@ -11,5 +20,21 @@ internal fun ItemsRoute() {
 
 @Composable
 internal fun ItemsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Feature in progress...")
+    }
+}
 
+@ThemePreviews
+@DevicesPreviews
+@Composable
+private fun ItemScreenPreview() {
+    LolTheme {
+        Surface {
+            ItemsScreen()
+        }
+    }
 }
